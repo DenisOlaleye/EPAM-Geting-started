0b6e28e7b0a0c5b626d475aa1a952d6e72194383
DenisOlaleye <denis.olaleye@gmail.com>
2022-08-22 21:53:53 +0300

    rewriting

---
+++ Exerciese.java
@@ -0,0 +1,18 @@
+
+public class Exerciese 
+{
+	public static void main(String[] args) 
+	{
+		int i = 10;	
+		if (i < 10)
+		{
+			System.out.println(1);
+		}
+		else if (i<=10)
+		{
+			System.out.println(2);
+		}
+		System.exit(i);
+		System.out.println(3);
+	}
+}
---
+++ FirstAplication.java
@@ -0,0 +1,14 @@
+
+public class FirstAplication 
+{
+	public static void main(String[] args) 
+	
+	{
+		System.out.println("First Java Aplication!");
+		System.out.println(42);
+		System.out.println("java" +11);;
+		System.out.println("\n Good job");
+		System.out.println("\t Nice evening)");
+	}
+
+}
